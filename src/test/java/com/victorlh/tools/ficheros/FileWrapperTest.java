233d98e6ba82caec464e58be7cb936b47681a54a
Victor Latorre <victor.latorre.herreros@gmail.com>
2019-10-13 14:10:32 +0200

    Añadidos test al proyecto

---
+++ src/test/java/com/victorlh/tools/ficheros/FileWrapperTest.java
@@ -0,0 +1,14 @@
+package com.victorlh.tools.ficheros;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileWrapperTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
