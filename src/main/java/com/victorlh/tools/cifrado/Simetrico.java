ab43d9e619f1e4e6be706705df07f8e9cbee17ac
Victor Latorre <victor.latorre.herreros@gmail.com>
2019-10-18 14:16:41 +0200

    Añadido JavaDoc a Simetrico

--- src/main/java/com/victorlh/tools/cifrado/Simetrico.java
+++ src/main/java/com/victorlh/tools/cifrado/Simetrico.java
@@ -1,7 +1,5 @@
 package com.victorlh.tools.cifrado;
 
-import java.nio.charset.StandardCharsets;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -10,27 +8,63 @@ import javax.crypto.spec.SecretKeySpec;
  * Created by victor on 16/06/2017.
  */
 
+/**
+ * Cifra y descifra información mediante el algoritmo de cifrado simetrico AES
+ * 
+ * @author VictorLh (<a href=
+ *         "https://github.com/victor-lh">https://github.com/victor-lh</a>)
+ *         created on 16 jun. 2017
+ *
+ */
 public class Simetrico {
 
 	private final static String AES = "AES";
 	private final static String MODO = "AES/CBC/PKCS5Padding";
 
-
+	/**
+	 * Encripta la información
+	 * 
+	 * @param key       - Clave con la que cifrar
+	 * @param iv        - vector de inicialización
+	 * @param cleartext - Texto que cifrar
+	 * @return resultado del cifrado del texto
+	 * @throws Exception - En caso de error al cifrar
+	 */
 	public static byte[] encriptar(String key, String iv, String cleartext) throws Exception {
-		Cipher cipher = Cipher.getInstance(MODO);
-		SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), AES);
-		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes(StandardCharsets.UTF_8));
-		cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
-		return cipher.doFinal(cleartext.getBytes(StandardCharsets.UTF_8));
+		return encriptar(key, iv, cleartext.getBytes());
 	}
 
-	public static String desencriptar(String key, String iv, byte[] encrypted) throws Exception {
+	/**
+	 * Encripta la información
+	 * 
+	 * @param key  - Clave con la que cifrar
+	 * @param iv   - vector de inicialización
+	 * @param data - información que cifrar
+	 * @return resultado del cifrado de la información
+	 * @throws Exception - En caso de error al cifrar
+	 */
+	public static byte[] encriptar(String key, String iv, byte[] data) throws Exception {
 		Cipher cipher = Cipher.getInstance(MODO);
-		SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), AES);
-		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes(StandardCharsets.UTF_8));
+		SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), AES);
+		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+		cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+		return cipher.doFinal(data);
+	}
+
+	/**
+	 * Desencripta la información
+	 * 
+	 * @param key       - Clave con la que se cifro el contenido
+	 * @param iv        - vector de inicialización
+	 * @param encrypted - información encriptada
+	 * @return Información desencriptada
+	 * @throws Exception - En caso de error al descifrar
+	 */
+	public static byte[] desencriptar(String key, String iv, byte[] encrypted) throws Exception {
+		Cipher cipher = Cipher.getInstance(MODO);
+		SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), AES);
+		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
 		cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivParameterSpec);
-		byte[] decrypted = cipher.doFinal(encrypted);
-		return new String(decrypted, StandardCharsets.UTF_8);
+		return cipher.doFinal(encrypted);
 	}
 }
-
